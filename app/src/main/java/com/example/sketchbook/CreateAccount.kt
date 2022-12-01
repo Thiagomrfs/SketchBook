a9a4d921ab094b9d1b725ec7fbc5c2dcd4383e43
Thiagomrfs <thiagomarbsn@gmail.com>
2022-12-01 18:15:34 -0300

    added navigation on login

--- app/src/main/java/com/example/sketchbook/CreateAccount.kt
+++ app/src/main/java/com/example/sketchbook/CreateAccount.kt
@@ -1,11 +1,18 @@
 package com.example.sketchbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CreateAccount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+    }
+
+    fun goToLogin(v: View){
+        val intent = Intent(this, MainActivity::class.java).apply{}
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/sketchbook/MainActivity.kt
+++ app/src/main/java/com/example/sketchbook/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.sketchbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToCreateAccount(v: View){
+        val intent = Intent(this, CreateAccount::class.java).apply{}
+        startActivity(intent)
     }
 }
