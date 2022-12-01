f7324e36d5eb65570d73cbab61d5272a901f121c
Thiagomrfs <thiagomarbsn@gmail.com>
2022-12-01 20:58:56 -0300

    get image intent

--- app/src/main/java/com/example/sketchbook/CreateAccount.kt
+++ app/src/main/java/com/example/sketchbook/CreateAccount.kt
@@ -1,14 +1,43 @@
 package com.example.sketchbook
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import java.net.URI
 
 class CreateAccount : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var profilePic: ImageView
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val response: Intent? = result.data
+            Log.d("PDM", response?.data?.path.toString())
+
+            profilePic.setImageURI(response?.data)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+        auth = FirebaseAuth.getInstance()
+
+        profilePic = findViewById(R.id.profileImage)
+    }
+
+    fun getImage(v: View) {
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        resultLauncher.launch(gallery)
     }
 
     fun goToLogin(v: View){
--- app/src/main/java/com/example/sketchbook/MainActivity.kt
+++ app/src/main/java/com/example/sketchbook/MainActivity.kt
@@ -33,18 +33,13 @@ class MainActivity : AppCompatActivity() {
         auth.signInWithEmailAndPassword(email.text.toString(), senha.text.toString())
             .addOnCompleteListener(this) { task ->
             if (task.isSuccessful) {
-                // Sign in success, update UI with the signed-in user's information
-                Log.d("PDM", "createUserWithEmail:success")
                 val user = auth.currentUser
                 Toast.makeText(baseContext, "Autenticação deu certo",
                     Toast.LENGTH_SHORT).show()
-                //updateUI(user)
             } else {
-                // If sign in fails, display a message to the user.
                 Log.w("PDM", "createUserWithEmail:failure", task.exception)
                 Toast.makeText(baseContext, "Autenticação falha",
                     Toast.LENGTH_SHORT).show()
-                //updateUI(null)
             }
         }
     }
