d6e736a909c42081686bc0c46a06a159c1784ede
Thiagomrfs <thiagomarbsn@gmail.com>
2022-12-02 20:14:39 -0300

    commentary cleaning

--- app/src/main/java/com/example/sketchbook/MainActivity.kt
+++ app/src/main/java/com/example/sketchbook/MainActivity.kt
@@ -29,18 +29,14 @@ class MainActivity : AppCompatActivity() {
         auth.signInWithEmailAndPassword(email, senha)
             .addOnCompleteListener(this) { task ->
             if (task.isSuccessful) {
-                // Sign in success, update UI with the signed-in user's information
                 Log.d("PDM", "createUserWithEmail:success")
                 val user = auth.currentUser
                 Toast.makeText(baseContext, "Autenticação deu certo",
                     Toast.LENGTH_SHORT).show()
-                //updateUI(user)
             } else {
-                // If sign in fails, display a message to the user.
                 Log.w("PDM", "createUserWithEmail:failure", task.exception)
                 Toast.makeText(baseContext, "Autenticação falha",
                     Toast.LENGTH_SHORT).show()
-                //updateUI(null)
             }
         }
     }
