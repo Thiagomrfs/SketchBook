e2d5d647f9f83e83414e94535301971baffe42c0
Thiagomrfs <thiagomarbsn@gmail.com>
2022-12-02 22:43:28 -0300

    getting user's itens on galery

--- app/src/main/java/com/example/sketchbook/Galery.kt
+++ app/src/main/java/com/example/sketchbook/Galery.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 
 class Galery : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -14,6 +18,32 @@ class Galery : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_galery)
         auth = FirebaseAuth.getInstance()
+
+        if(auth.currentUser == null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val mydb = FirebaseDatabase.getInstance().reference
+        val desenhos = mydb.child("desenhos")
+
+        val desenhosFiltrados = desenhos
+            .orderByChild("usuario")
+            .equalTo(auth.currentUser?.uid.toString())
+
+//        desenhosFiltrados.addChildEventListener(object : ChildEventListener {
+//            override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {}
+//            override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {}
+//            override fun onChildRemoved(dataSnapshot: DataSnapshot) {}
+//            override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {}
+//            override fun onCancelled(databaseError: DatabaseError) {}
+//        })
+
+        desenhosFiltrados.get().addOnSuccessListener {
+            Log.d("PDM", "Got value ${it.value}")
+        }.addOnFailureListener{
+            Log.d("PDM", "Error getting data", it)
+        }
     }
 
     fun goToCriarDesenho(v: View){
