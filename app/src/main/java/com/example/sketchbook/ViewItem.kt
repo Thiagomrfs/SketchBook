8642abd1d2ff67134ee12e46202b569f000a2a71
Thiagomrfs <thiagomarbsn@gmail.com>
2022-12-04 15:48:08 -0300

    add images on view item

--- app/src/main/java/com/example/sketchbook/ViewItem.kt
+++ app/src/main/java/com/example/sketchbook/ViewItem.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class ViewItem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_item)
+
+        val storageReference = Firebase.storage.reference
+
 
         //Elementos do layout
         val itemName = findViewById<EditText>(R.id.viewItemName)
@@ -23,5 +29,8 @@ class ViewItem : AppCompatActivity() {
         itemCategoria.setText(intent.getStringExtra("categoria"))
         itemPreco.setText(intent.getStringExtra("preco"))
         itemDescricao.setText(intent.getStringExtra("descricao"))
+        GlideApp.with(this /* context */)
+            .load(storageReference.child("imagens").child(intent.getStringExtra("imagem").toString()))
+            .into(itemImage)
     }
 }
